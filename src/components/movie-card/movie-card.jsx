4e02500bf7f785a26bb8ebdacf6706b0228fa6e5
lenachat <124374456+lenachat@users.noreply.github.com>
2024-12-29 16:46:44 +0100

    code clean up
    - remove uncommented code snippets

--- src/components/movie-card/movie-card.jsx
+++ src/components/movie-card/movie-card.jsx
@@ -1,63 +1,12 @@
 import PropTypes from "prop-types";
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import { Button, ButtonGroup, ToggleButton, Card } from "react-bootstrap";
 import { Link } from "react-router-dom";
 import { useFavorites } from "../../hooks/useFavorites";
 
 import "./movie-card.scss";
-import { useEffect, useState } from "react";
 
 export const MovieCard = ({ movie, user, onFavoriteAdded, onFavoriteRemoved }) => {
-  // const [isFavorite, setIsFavorite] = useState(false);
-
-  // useEffect(() => {
-  //   if (user && user.favorites) {
-  //     setIsFavorite(user.favorites.includes(movie.id));
-  //   }
-  // }, [user, movie.id]);
-
-  // const handleToggleFavorite = () => {
-  //   if (isFavorite) {
-  //     handleRemoveFromFavorites();
-  //   } else {
-  //     handleAddToFavorites();
-  //   }
-  //   setIsFavorite(!isFavorite);
-  // }
-
-  // const handleAddToFavorites = () => {
-  //   fetch(`https://moviemate-mk9e.onrender.com/users/${user._id}/favorites/${movie.id}`, {
-  //     method: 'POST',
-  //     headers: {
-  //       'Content-Type': 'application/json',
-  //       Authorization: `Bearer ${localStorage.getItem('token')}`
-  //     }
-  //   }).then((response) => response.json())
-  //     .then((data) => {
-  //       onFavoriteAdded(data);
-  //       setIsFavorite(true);
-  //       console.log('Movie added to favorites: ', data);
-  //     }).catch((error) => {
-  //       console.error('Error adding movie to favorites: ', error.message);
-  //     });
-  // };
-
-  // const handleRemoveFromFavorites = () => {
-  //   fetch(`https://moviemate-mk9e.onrender.com/users/${user._id}/favorites/${movie.id}`, {
-  //     method: 'DELETE',
-  //     headers: {
-  //       'Content-Type': 'application/json',
-  //       Authorization: `Bearer ${localStorage.getItem('token')}`
-  //     }
-  //   }).then((response) => response.json())
-  //     .then((data) => {
-  //       onFavoriteRemoved(data);
-  //       setIsFavorite(false);
-  //       console.log('Movie removed from favorites: ', data);
-  //     }).catch((error) => {
-  //       console.error('Error removing movie from favorites: ', error.message);
-  //     });
-  // };
 
   const { isFavorite, handleToggleFavorite } = useFavorites(movie, user, onFavoriteAdded, onFavoriteRemoved);
 
